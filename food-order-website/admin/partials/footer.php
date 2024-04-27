e963c07beef93627d743a0241a48abc76d0732b2
Raghav Mahajan ❤️ <97866300+raghavmahajan821@users.noreply.github.com>
2024-04-28 01:39:13 +0530

    Add files via upload

---
+++ food-order-website/admin/partials/footer.php
@@ -0,0 +1,13 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+    <div class="wrapper">
+        <p class="text-center">2024 All rights reserved, Hunter Foods. </p>
+        <br>
+        <p class="text-center">Developed By - Raghav Mehak</p>
+    </div>
+</div>
+<!-- Footer Section Ends -->
+
+</body>
+
+</html>
